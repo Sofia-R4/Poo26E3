bb05dbb5be2d4db16b22e513b812f91e19d97db7
Beatriz Pimentel <Beatriz Pimentel@Lua>
2026-05-17 22:13:09 +0100

    Gets and sets

--- PooE3/src/PooE3/User.java
+++ PooE3/src/PooE3/User.java
@@ -17,9 +17,25 @@ public class User {
 		this.name = name;
 		this.password = password;
 	}
-	
-	
 
-	
-	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
