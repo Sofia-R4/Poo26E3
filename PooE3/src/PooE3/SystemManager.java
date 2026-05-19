37e1d589364fc66f00a60154443d4513afe64c79
Carlos <grimaldos882@gmail.com>
2026-05-19 14:24:46 +0100

    Addition of methods to Sys.Manager and helper methods.

--- PooE3/src/PooE3/SystemManager.java
+++ PooE3/src/PooE3/SystemManager.java
@@ -7,6 +7,8 @@ package PooE3;
  * 
  */
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.time.LocalDate;
 
 
@@ -42,10 +44,10 @@ public class SystemManager {
 	//add an user
 	public void addUser(User user) {
 		users.add(user);
-		System.out.println("User add with succes!");
+		System.out.println("User add with success!");
 	}
-	
-	//test if password is vallid
+		
+	//test if password is valid
 	public boolean isPasswordValid(String password) {
 		if(password.length()<8) {
 			System.out.println("The password must have at least 8 characters.");
@@ -132,6 +134,77 @@ public class SystemManager {
 	//add employee, only admin can do it
 	public void addEmployee(Employee employee) {
 		employees.add(employee);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	// Method to get all occurrences
+
+	public ArrayList<Occurrence> getOccurrences() {
+		return new ArrayList<>(occurrences);
+	}
+	
+	
+	// Method to get all employees by specialty
+	
+	public ArrayList<Employee> getEmployeesBySpecialty(Specialty specialty){
+		
+		ArrayList<Employee> result = new ArrayList();
+		
+		if(specialty == null) {
+			System.out.println("Specialty cannot be null!");
+			return result;
+		}
+		
+		for(Employee employee : employees) {
+			if(employee.getSpecialty() !=  null && employee.getSpecialty().equals(specialty)) {
+				result.add(employee);
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	// Method to get all occurrence totals grouped by type
+	public Map<OccurrenceType, Integer> getOccurrenceTotalsByType(){
+		
+		Map<OccurrenceType, Integer> totals = new HashMap<>();
+		
+		for(Occurrence occurrence : occurrences) {
+			OccurrenceType type = occurrence.getType();
+			
+			if(type != null) {
+				if(totals.containsKey(type)){
+					totals.put(type, totals.get(type) + 1);
+				} else {
+					totals.put(type, 1);
+				}
+			}
+		}
+		
+		return totals;
+		
+	}
+	
+	
+	
+	
+	
+	// Method to add a Specialty
+	public void addSpecialty(Specialty specialty) {
+		if(specialty == null) {
+			System.out.println("Specialty cannot be null!");
+			return;
+		}
+		
+		specialties.add(specialty);
+		System.out.println("Specialty added successfully");
 	}
 	
 }
