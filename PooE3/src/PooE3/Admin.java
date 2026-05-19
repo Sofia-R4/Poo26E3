a1ac77ac45f1df8bd3e5ce3283ee78f78d866b56
Sofia <beatrizpimentelribeiro@gmail.com>
2026-05-19 21:34:54 +0100

    register employee and remove Occurrence methods

--- PooE3/src/PooE3/Admin.java
+++ PooE3/src/PooE3/Admin.java
@@ -3,6 +3,8 @@
  */
 package PooE3;
 
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -22,6 +24,23 @@ public class Admin extends User{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	//method to add an employee
+	public void registerEmployee(ArrayList<User> users, Employee employee) {
+        users.add(employee);
+    }
+	
+	//remove occurrence method
+	public void removeOccurrence(ArrayList<Occurrence> occurrences, Occurrence occurrence) {
+		if(occurrence.getStatus() == OccurrenceStatus.PENDING) {
+			occurrences.remove(occurrence);
+			System.out.println("Ocorrência removida com sucesso!");
+		}
+		
+		else {
+			System.out.println("A ocorrência não pode ser removida pois já não está no estado Pendente.");
+		}
 	}
 	
 }
