9f50147df703871823ed0f1ab7b738364fb02a76
Carlos <grimaldos882@gmail.com>
2026-06-10 09:15:11 +0100

    Comment class general fix. Simplifying two constructors into one...

--- PooE3/src/PooE3/Comment.java
+++ PooE3/src/PooE3/Comment.java
@@ -10,9 +10,12 @@ import java.time.LocalDate;
 
 public class Comment {
 	private int id;
+	private static int nextId = 1;
+	
 	private String text;
 	private LocalDate postedDate;
 	private User author;
+	
 	/**
 	 * @param id
 	 * @param text
@@ -21,44 +24,66 @@ public class Comment {
 	 */
 	
 	
-	public Comment(int id, String text, LocalDate postedDate, User author) {
-		super();
-		this.id = id;
+	public Comment(String text, User author) {
+		
+        if (text == null || text.isBlank()) {
+            System.out.println("Error: comment text cannot be empty.");
+            return;
+        }
+
+        if (author == null) {
+            System.out.println("Error: comment author cannot be null.");
+            return;
+        }
+        
+		this.id = nextId++;
 		this.text = text;
-		this.postedDate = postedDate;
+		this.postedDate = LocalDate.now();
 		this.author = author;
 	}
 	
-	public Comment(String text, User author) {  //metodo addcomment
-	    this.text = text;
-	    this.author = author;
-	    this.postedDate = LocalDate.now();
-	}
 	
 	
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getText() {
 		return text;
 	}
+	
 	public void setText(String text) {
+		 if (text == null || text.isBlank()) {
+	            System.out.println("Error: comment text cannot be empty.");
+	            return;
+	        }
+		 
 		this.text = text;
 	}
+	
 	public LocalDate getPostedDate() {
 		return postedDate;
 	}
-	public void setPostedDate(LocalDate postedDate) {
-		this.postedDate = postedDate;
-	}
+	
 	public User getAuthor() {
 		return author;
 	}
+	
 	public void setAuthor(User author) {
+        if (author == null) {
+            System.out.println("Error: comment author cannot be null.");
+            return;
+        }
+        
 		this.author = author;
 	}
 	
+	
+	 @Override
+	    public String toString() {
+	        return "Comment ID: " + id +
+	                "\nText: " + text +
+	                "\nPosted date: " + postedDate +
+	                "\nAuthor: " + author.getName();
+	    }
 }
